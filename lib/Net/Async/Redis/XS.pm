999221ea148bf0c1b42a13999ee533e6a5e52e43
Tom Molesworth <TEAM@cpan.org>
2022-12-01 11:29:21 +0800

    Synopsis and one-line name description

--- lib/Net/Async/Redis/XS.pm
+++ lib/Net/Async/Redis/XS.pm
@@ -1,5 +1,5 @@
 package Net::Async::Redis::XS;
-# ABSTRACT: faster
+# ABSTRACT: faster version of Net::Async::Redis
 
 use strict;
 use warnings;
@@ -7,6 +7,33 @@ use warnings;
 our $VERSION = '0.001';
 
 use parent qw(Net::Async::Redis);
+
+=head1 NAME
+
+Net::Async::Redis::XS - like L<Net::Async::Redis> but faster
+
+=head1 SYNOPSIS
+
+ use feature qw(say);
+ use Future::AsyncAwait;
+ use IO::Async::Loop;
+ use Net::Async::Redis::XS;
+ my $loop = IO::Async::Loop->new;
+ $loop->add(my $redis = Net::Async::Redis::XS);
+ await $redis->connect;
+ await $redis->set('some-key', 'some-value');
+ say await $redis->get('some-key');
+
+=head1 DESCRIPTION
+
+This is a wrapper around L<Net::Async::Redis> with faster protocol parsing.
+
+It implements the L<Net::Async::Redis::Protocol> protocol code in XS for better performance,
+and will eventually be extended to optimise some other slow paths as well in future.
+
+API and behaviour should be identical to L<Net::Async::Redis>, see there for instructions.
+
+=cut
 
 package Net::Async::Redis::Protocol::XS {
     use parent qw(Net::Async::Redis::Protocol);
@@ -36,4 +63,12 @@ sub wire_protocol {
 }
 
 1;
+
+=head1 AUTHOR
+
+Tom Molesworth <TEAM@cpan.org>
+
+=head1 LICENSE
+
+Copyright Tom Molesworth 2022. Licensed under the same terms as Perl itself.
 
