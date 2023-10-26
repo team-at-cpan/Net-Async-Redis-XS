84c9313e406ad7e8baf9d7a9e25bb51678fdd766
Tom Molesworth <TEAM@cpan.org>
2023-10-26 23:06:09 +0800

    Version 0.014

--- lib/Net/Async/Redis/Cluster/XS.pm
+++ lib/Net/Async/Redis/Cluster/XS.pm
@@ -3,7 +3,7 @@ package Net::Async::Redis::Cluster::XS;
 use strict;
 use warnings;
 
-our $VERSION = '0.013'; # VERSION
+our $VERSION = '0.014'; # VERSION
 
 use parent qw(Net::Async::Redis::Cluster);
 
